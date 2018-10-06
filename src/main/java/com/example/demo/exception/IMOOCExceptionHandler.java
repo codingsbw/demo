50f1aa23406d70e1b9515de44bd28e3b3ac4191c
sbw <mailsbw@163.com>
2018-10-06 20:32:26 +0800

    merge web and ajax error

--- src/main/java/com/example/demo/exception/IMOOCExceptionHandler.java
+++ src/main/java/com/example/demo/exception/IMOOCExceptionHandler.java
@@ -7,18 +7,37 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.example.demo.utils.AjaxUtil;
+import com.example.demo.utils.IMoocJSONResult;
+
 @ControllerAdvice
 public class IMOOCExceptionHandler {
-	public static final String IMOOC_ERROR_VIEW="thymeleaf/error";
-	@ExceptionHandler(value=Exception.class)
+	public static final String IMOOC_ERROR_VIEW = "thymeleaf/error";
+
+//	@ExceptionHandler(value = Exception.class)
+//	public Object errorHandler(HttpServletRequest reqest, HttpServletResponse response, Exception e) throws Exception {
+//
+//		e.printStackTrace();
+//
+//		ModelAndView mav = new ModelAndView();
+//		mav.addObject("exception", e);
+//		mav.addObject("url", reqest.getRequestURL());
+//		mav.setViewName(IMOOC_ERROR_VIEW);
+//		return mav;
+//	}
+	@ExceptionHandler(value = Exception.class)
 	public Object errorHandler(HttpServletRequest reqest, HttpServletResponse response, Exception e) throws Exception {
 
 		e.printStackTrace();
 
-		ModelAndView mav = new ModelAndView();
-		mav.addObject("exception", e);
-		mav.addObject("url", reqest.getRequestURL());
-		mav.setViewName(IMOOC_ERROR_VIEW);
-		return mav;
+		if (AjaxUtil.isAjax(reqest)) {
+			return IMoocJSONResult.errorException(e.getMessage());
+		} else {
+			ModelAndView mav = new ModelAndView();
+			mav.addObject("exception", e);
+			mav.addObject("url", reqest.getRequestURL());
+			mav.setViewName(IMOOC_ERROR_VIEW);
+			return mav;
+		}
 	}
 }
---
+++ src/main/java/com/example/demo/utils/AjaxUtil.java
@@ -0,0 +1,21 @@
+package com.example.demo.utils;
+
+import javax.servlet.http.HttpServletRequest;
+
+public class AjaxUtil {
+	/**
+	 * 
+	 * @Title: IMoocExceptionHandler.java
+	 * @Package com.imooc.exception
+	 * @Description: 判断是否是ajax请求 Copyright: Copyright (c) 2017
+	 *               Company:FURUIBOKE.SCIENCE.AND.TECHNOLOGY
+	 * 
+	 * @author leechenxiang
+	 * @date 2017年12月3日 下午1:40:39
+	 * @version V1.0
+	 */
+	public static boolean isAjax(HttpServletRequest httpRequest) {
+		return (httpRequest.getHeader("X-Requested-With") != null
+				&& "XMLHttpRequest".equals(httpRequest.getHeader("X-Requested-With").toString()));
+	}
+}
